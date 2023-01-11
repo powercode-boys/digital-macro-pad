1dcffdd199f7cd6f8f44d188f80917193d4cf5db
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-11 14:07:24 +0100

    add baisc electron window

---
+++ src/electron/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  if (process.env.NODE_ENV !== "development") {
+    // Load production build
+    win.loadFile(`${__dirname}/../svelte/dist/index.html`);
+  } else {
+    // Load vite dev server page
+    win.loadURL("http://localhost:5173/");
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
