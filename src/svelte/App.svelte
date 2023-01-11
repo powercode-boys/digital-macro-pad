3878c7d2ba42e6a9c0127ddec65b7f54259c130d
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-11 14:27:13 +0100

    add tailwindcss

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -2,16 +2,11 @@
 </script>
 
 <main>
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <button class="btn"></button>
 </main>
 
-<style>
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
