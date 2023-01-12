64c90e562e521a4e74eddf8903c2a34f93724c12
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-12 10:30:02 +0100

    style and adjust add button

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -2,31 +2,35 @@
   let buttons = Array(9);
 </script>
 
-<div
-  class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4"
->
-  {#each buttons as button}
-    <button class="btn btn-lg btn-primary">Makro</button>
-  {/each}
-</div>
-
-<button
-  class="btn btn-circle btn-outline fixed bottom-5 right-5 sm:right-[5vw] sm:bottom-[5vh]"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-8 w-8"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    ><path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M6 18L18 6M6 6l12 12"
-    /></svg
+<main>
+  <div
+    class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4"
   >
-</button>
+    {#each buttons as button}
+      <button class="btn btn-lg btn-primary">Makro</button>
+    {/each}
+  </div>
+
+  <div class="tooltip fixed bottom-5 right-5 sm:right-[5vw] sm:bottom-[5vh]" data-tip="Makro hinzufügen">
+    <button
+      class="btn btn-circle btn-outline btn-success"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2.5"
+        stroke="currentColor"
+        class="w-8 h-8 "
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+        />
+      </svg>
+    </button>
+  </div>
+</main>
 
 <style global lang="postcss">
   @tailwind base;
