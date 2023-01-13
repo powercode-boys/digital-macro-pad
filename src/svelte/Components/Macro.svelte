fa962258a286255e2a9cab1227bbdbc2548abdcb
eturan <ediz.turan@eves-it.de>
2023-01-13 16:47:22 +0100

    click on button copies to clipboard

--- src/svelte/Components/Macro.svelte
+++ src/svelte/Components/Macro.svelte
@@ -1,8 +1,9 @@
 <script>
   export let macro;
+
 </script>
 
-<button>
+<button on:click={() => navigator.clipboard.writeText(macro.command)}>
   <div class="card bg-primary text-primary-content p-4 hover:bg-opacity-80 h-full">
     <div class="card-body text-start">
       <h2 class="card-title">{macro.name}</h2>
