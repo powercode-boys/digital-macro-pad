a9deae7891284f58f374fad36c46c0bb0bf451f1
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-16 15:21:48 +0100

    nacharbeiten done

--- src/electron/index.cjs
+++ src/electron/index.cjs
@@ -6,6 +6,8 @@ function createWindow() {
     height: 600,
   });
 
+  win.setMenu(null)
+
   if (process.env.NODE_ENV !== "development") {
     // Load production build
     win.loadFile(`${__dirname}/../svelte/dist/index.html`);
