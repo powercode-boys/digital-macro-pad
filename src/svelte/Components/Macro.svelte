37e51e99628da0675cfe8be7bc76e14c1087cba3
eturan <ediz.turan@eves-it.de>
2023-01-16 15:04:27 +0100

    visual feedback when copying

--- src/svelte/Components/Macro.svelte
+++ src/svelte/Components/Macro.svelte
@@ -1,13 +1,27 @@
 <script>
-  export let macro;
+    export let macro;
+    const clipboardIcon = '&#128203;';
+    let showConfirmation = false;
+
+    function copyCommand() {
+        showConfirmation = true;
+        navigator.clipboard.writeText(macro.command);
+        setTimeout(() => showConfirmation = false, 1500);
+    }
 
 </script>
 
-<button on:click={() => navigator.clipboard.writeText(macro.command)}>
-  <div class="card bg-primary text-primary-content p-4 hover:bg-opacity-80 h-full">
-    <div class="card-body text-start">
-      <h2 class="card-title">{macro.name}</h2>
-      <p>{macro.description}</p>
+<button>
+    <div class="card bg-primary text-primary-content p-4 hover:bg-opacity-80 h-full">
+        <div class="card-body text-start">
+            <h2 class="card-title overflow-clip">{macro.name}</h2> <!-- Title doesn't clip -->
+            <p class="break-all">{macro.description}</p>
+        </div>
+        <div id="copyButton" class="flex items-end justify-end ">
+            <div class={showConfirmation ? 'tooltip tooltip-open tooltip-secondary' : ''} data-tip="Copied!">
+                <button class="btn btn-circle btn-secondary text-xl"
+                        on:click={copyCommand}>{@html clipboardIcon}</button>
+            </div>
+        </div>
     </div>
-  </div>
 </button>
