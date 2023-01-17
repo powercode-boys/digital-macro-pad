a9b84fb86f9b25432cd35cad1775667a0b9ff8e0
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-17 12:01:26 +0100

    added new macro layout

--- src/svelte/Components/Macro.svelte
+++ src/svelte/Components/Macro.svelte
@@ -1,30 +1,55 @@
 <script>
   import { removeMacro } from "../stores";
+  import MacroActionButton from "./MacroActionButton.svelte";
 
-
-    export let macro;
-    export let idx;
-
-    const clipboardIcon = '&#128203;';
-    let confirmPopup = false;
-
-    function copyCommand() {
-        confirmPopup = true;
-        navigator.clipboard.writeText(macro.command);
-        setTimeout(() => confirmPopup = false, 1500);
-    }
-
+  export let macro;
+  export let idx;
 </script>
 
 <div class="card bg-primary text-primary-content p-4 h-full">
-    <div class="card-body text-start">
-        <h2 class="card-title overflow-clip break-words">{macro.name}</h2> <!-- Title doesn't clip -->
-        <p class="break-words italic">{macro.description}</p>
+  <div class="flex h-full">
+    <div class="card-body">
+      <h2 class="card-title overflow-clip break-words">{macro.name}</h2>
+      <p class="break-words italic">{macro.description}</p>
     </div>
-    <div id="copyButton" class="flex items-end justify-end">
-        <div class={confirmPopup ? 'tooltip tooltip-open tooltip-secondary' : ''} data-tip="Copied!">
-            <button class="btn btn-circle btn-secondary text-xl"
-                    on:click={copyCommand}>{@html clipboardIcon}</button>
-        </div>
+
+    <div class="card-actions">
+      <button class="btn btn-circle btn-primary"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+          />
+        </svg>
+      </button>
     </div>
+  </div>
+  <div class="card-actions justify-end">
+    <MacroActionButton
+      tooltip="Copied!"
+      action={() => navigator.clipboard.writeText(macro.command)}
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+        />
+      </svg>
+    </MacroActionButton>
+  </div>
 </div>
