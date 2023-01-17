9c8fd1b63903f085902c5b65015e56a14cc559e8
Laurenz1606 <laurenz.rausche@gmail.com>
2023-01-17 13:53:39 +0100

    change default window h & w for 3x2 macros

--- src/electron/index.cjs
+++ src/electron/index.cjs
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1050,
+    height: 605,
   });
 
   if (process.env.NODE_ENV !== "development") {
